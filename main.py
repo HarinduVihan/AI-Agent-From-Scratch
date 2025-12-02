e272f2e2848c3fc212ef30d3a8f03f320b1de26d
haribduvihan <harinduvihan@gmail.com>
2025-12-02 13:03:57 +0530

    remove unwanted commets

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel
-# from langchain_openai import ChatOpenAI
-# from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
@@ -9,11 +7,6 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from tools import search_tool, wiki_tool, save_tool
 
 load_dotenv()
-
-# #llm = ChatOpenAI(model="gpt-4")
-# llm = ChatAnthropic(model="claude-3-5-sonnet-202410022")
-# response = llm.invoke("What is the meaning of life?")
-# print(response)
 
 from google import genai
 
